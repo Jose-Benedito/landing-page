647b7d1296b25759fec63093b3a09593eed5f385
jose-benedito <benethowen@yahoo.com.br>
2024-10-22 15:01:00 -0300

    Estilos no fornulário e função de validação no botão submit

--- src/app/components/newsletter-form/newsletter-form.component.ts
+++ src/app/components/newsletter-form/newsletter-form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-newsletter-form',
   standalone: true,
-  imports: [],
+  imports: [BtnPrimaryComponent,
+           ReactiveFormsModule],
   templateUrl: './newsletter-form.component.html',
   styleUrl: './newsletter-form.component.scss'
 })
 export class NewsletterFormComponent {
+
+  newsletterform: FormGroup;
+
+  constructor(){
+    this.newsletterform = new FormGroup ({
+
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email])
+
+    });
+  }
+
+  Onsubmit(){
+    console.log(this.newsletterform.value);
+  }
 
 }
